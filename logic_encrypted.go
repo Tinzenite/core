ebe7643a3efdb9d290ace4f176d2bcff9a0f9ecc
Tamino Hartmann <tamino.hartmann@gmail.com>
2015-10-01 16:33:26 +0200

    comments and lock functionality

--- logic_encrypted.go
+++ logic_encrypted.go
@@ -48,6 +48,10 @@ func (c *chaninterface) onEncryptedMessage(address string, msgType shared.MsgTyp
 	}
 }
 
+/*
+onEncLockMessage handles lock messages. Notably it requests a model on a
+successful lock, starting a synchronization.
+*/
 func (c *chaninterface) onEncLockMessage(address string, msg shared.LockMessage) {
 	switch msg.Action {
 	case shared.LoAccept:
@@ -61,6 +65,14 @@ func (c *chaninterface) onEncLockMessage(address string, msg shared.LockMessage)
 		// if LOCKED request model file to begin sync
 		rm := shared.CreateRequestMessage(shared.OtModel, shared.IDMODEL)
 		c.tin.channel.Send(address, rm.JSON())
+	case shared.LoRelease:
+		// unset lock of this peer
+		_, exists := c.tin.peers[address]
+		if !exists {
+			c.warn("Can not release peer as peer doesn't exist!")
+			return
+		}
+		c.tin.peers[address].SetLocked(false)
 	default:
 		c.warn("Unknown lock action received:", msg.Action.String())
 	}
@@ -82,13 +94,18 @@ func (c *chaninterface) onEncNotifyMessage(address string, msg shared.NotifyMess
 			c.sendCompletePushes(address)
 			return
 		}
-		// if object --> error... maybe "reset" the encrypted peer?
+		// TODO: if object --> error... maybe "reset" the encrypted peer?
 		log.Println("DEBUG: object missing!", msg.Identification, msg.Notify)
+		log.Println("DEBUG: TODO: reset encrypted peer?")
 	default:
 		c.warn("Unknown notify type received:", msg.Notify.String())
 	}
 }
 
+/*
+onEncRequestMessage handles the reception of a request message following a push
+message. Triggers the sending of the requested file.
+*/
 func (c *chaninterface) onEncRequestMessage(address string, msg shared.RequestMessage) {
 	var path string
 	// if model has been requested --> path is different as not tracked itself
@@ -103,7 +120,7 @@ func (c *chaninterface) onEncRequestMessage(address string, msg shared.RequestMe
 		}
 		path = c.tin.Path + "/" + subPath
 	}
-	// and send file
+	// and send file (concurrent because of encryption)
 	go c.encSend(address, msg.Identification, path, msg.ObjType)
 	// TODO: shouldn't we reread the msg.ObjType from disk too?
 }
@@ -120,7 +137,7 @@ func (c *chaninterface) encSend(address, identification, path string, ot shared.
 		c.warn("Failed to read data:", err.Error())
 		return
 	}
-	// TODO encrypt here?
+	// TODO encrypt here? FIXME
 	// log.Println("DEBUG: encrypt here, and once done, send if time since timeout is larger!")
 	// write to temp file
 	sendPath := c.tin.Path + "/" + shared.TINZENITEDIR + "/" + shared.SENDINGDIR + "/" + identification
@@ -147,6 +164,10 @@ func (c *chaninterface) encSend(address, identification, path string, ot shared.
 	// done
 }
 
+/*
+sendCompletePushes sends push models for everything, starting with the model.
+This will result in the encrypted peer requesting all objects.
+*/
 func (c *chaninterface) sendCompletePushes(address string) {
 	// vars we'll use
 	var pm shared.PushMessage
